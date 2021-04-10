8c046ac7c25b83e6b456ff4f48102521a7aa2462
yperez0914 <71104698+yperez0914@users.noreply.github.com>
2021-04-10 16:03:19 -0400

    updated API and start MongoDB conxn

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-from flask_jsonpify import jsonpify
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 import pymongo
 import pandas as pd 
 import requests
 import json
-from config import YP_API_KEY
+from config import YP_API_KEY, lw_key
 from datetime import date, timedelta
 # import geopy
 import pgeocode
@@ -15,38 +15,33 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 #create function for API call in route
 @app.route("/")
 def index():
-    url= "https://api.openweathermap.org/data/2.5/onecall?"
-    file = "mdzips.csv"
-    df = pd.read_csv(file)
-    md_zips = df['ZIPCODE1'].apply(nomi.query_postal_code)
-    weather_dict ={
-    "Zipcode": [],
-    "Dates":[],
-    "Pressure":[]
-    }
-
-    for i in range(len(md_zips.index)):
-        response = requests.get(f"{url}lat={md_zips['latitude'][i]}&lon={md_zips['longitude'][i]}&exclude=current,minutely,hourly,alerts&appid={YP_API_KEY}").json()
-        weather_dict["Zipcode"].append(md_zips["postal_code"][i])
-        weather_dict["Dates"].append([response["daily"][i]["dt"] for i in range(len(response["daily"]))])
-        weather_dict["Pressure"].append([response["daily"][i]["pressure"] for i in range(len(response["daily"]))])
-        
-    weather_df =pd.DataFrame.from_dict(weather_dict, orient = 'index')
-    weather_df = weather_df.transpose()
-    weather_df[[f"{date.today()}", f"{date.today()+timedelta(1)}", f"{date.today()+timedelta(2)}", f"{date.today()+timedelta(3)}", f"{date.today()+timedelta(4)}", f"{date.today()+timedelta(5)}", f"{date.today()+timedelta(6)}", f"{date.today()+timedelta(7)}"]]= pd.DataFrame(weather_df.Pressure.to_list(), index = weather_df.index)
-    forecast = weather_df.drop(columns= ["Dates","Pressure"])
-    result = json.loads(forecast.to_json(orient = 'records'))
-    weather_api = json.dumps(result, indent =200)
-    return (weather_api)
+   url= "https://api.worldweatheronline.com/premium/v1/weather.ashx?"
+   zipcode = input("Enter 5-digit Zipcode:")
+   response = requests.get(f"{url}key={lw_key}&q={zipcode}&num_of_days=7&tp=24&mca=no&aqi=yes&format=json").json()
+    # result = json.loads(forecast.to_json(orient = 'records'))
+   forecast_api = json.dumps(response, indent =200)
+   return (forecast_api)
     # return render_template('index.html', weather_api=weather_api)
-    
-    # result = forecast.values.tolist()
-    # JSONP_data= jsonpify(result)
-    # return(JSONP_data) 
-    # return jsonify()
+
 # setup mongo connection
 # conn = "mongodb://localhost:27017"
 # client = pymongo.MongoClient(conn)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/forecast_db"
+mongo = PyMongo(app)
+
+@app.route("/forecast_data")
+def forecast_load():
+    forecast_db = mongo.db.forecast_db
+    collection = forecast_db.final
+    forecast_data = index()
+    forecast_db.update({}, forecast_data, upsert=True)
+    return redirect("/", code=302)
+
+
+
+
 
 # # connect to mongo db and collection
 # db = client.store_inventory
