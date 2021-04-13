29280e286f85f72a8149b62a786e7920e206ecfa
yperez0914 <71104698+yperez0914@users.noreply.github.com>
2021-04-12 21:30:38 -0400

    MongoDB database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import pymongo
 import pandas as pd 
@@ -12,37 +12,37 @@ nomi = pgeocode.Nominatim('US')
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-#create function for API call in route
-@app.route("/")
-def index():
-   url= "https://api.worldweatheronline.com/premium/v1/weather.ashx?"
-   zipcode = input("Enter 5-digit Zipcode:")
-   response = requests.get(f"{url}key={lw_key}&q={zipcode}&num_of_days=7&tp=24&mca=no&aqi=yes&format=json").json()
-    # result = json.loads(forecast.to_json(orient = 'records'))
-   forecast_api = json.dumps(response, indent =200)
-   return (forecast_api)
-    # return render_template('index.html', weather_api=weather_api)
-
-# setup mongo connection
-# conn = "mongodb://localhost:27017"
-# client = pymongo.MongoClient(conn)
-
-# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/forecast_db"
 mongo = PyMongo(app)
-
+#create function for API call in route
 @app.route("/forecast_data")
 def forecast_load():
-    forecast_db = mongo.db.forecast_db
-    collection = forecast_db.final
-    forecast_data = index()
-    forecast_db.update({}, forecast_data, upsert=True)
-    return redirect("/", code=302)
+    url= "https://api.worldweatheronline.com/premium/v1/weather.ashx?"
+    zipcode = input("Enter 5-digit Zipcode:")
+    response = requests.get(f"{url}key={lw_key}&q={zipcode}&num_of_days=7&tp=24&mca=no&aqi=yes&format=json").json()
+    # result = json.loads(forecast.to_json(orient = 'records'))
+    # forecast_api = json.dumps(response, indent =200)
+    #    return (forecast_api)
+    # return render_template('index.html', weather_api=weather_api)
 
+    # Use flask_pymongo to set up mongo connection
+    collection = mongo.db.final
+    collection.insert_one(response)
+    print("Data inserted")
+    return "Try it" # jsonify(response)
 
-
-
-
+@app.route("/history")
+def history():
+    past_url = "https://api.worldweatheronline.com/premium/v1/past-weather.ashx?"
+    zipcode = input("Enter 5-digit Zipcode:")
+    dates= input("Dates you had a migraine (YYYY-MM-DD):")
+    dates_split = dates.split(",")
+    for date in dates_split:
+        responses = requests.get(f"{past_url}key={lw_key}&q={zipcode}&date={date}&tp=24&mca=no&aqi=yes&format=json").json()
+    collection = mongo.db.history
+    collection.insert_one(responses)
+    print("Data inserted")
+    return "History"
 # # connect to mongo db and collection
 # db = client.store_inventory
 # produce = db.produce
