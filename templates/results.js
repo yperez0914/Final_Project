32f5741f2b8c2a277996dedc1761ee7a4b7791f2
Kevin Ricker <kevincharlesricker@gmail.com>
2021-04-17 18:05:49 -0400

    template_update

--- templates/results.js
+++ templates/results.js
@@ -1,27 +1,136 @@
+var data = [{
 
+  "Dates": "2021-04-18",
+  "Cloudcover": "62",
+  "Humidity": "72",
+  "Pressure": "1014",
+  "FeelsLike": "44",
+  "HeatIndex": "46",
+  "MaxTemp": "54",
+  "MinTemp": "38",
+  "SunHours":"9.1",
+  "UVIndex":"5",
+  "TempDelta":"16",
+  "BarChange":"0.003",
+  "HeatChange":"0.022",
+  "HumChange":"-0.014"
+}, {
 
-console.log({hello:"kevin"})
+  "Dates": "2021-04-18",
+  "Cloudcover": "62",
+  "Humidity": "72",
+  "Pressure": "1014",
+  "FeelsLike": "44",
+  "HeatIndex": "46",
+  "MaxTemp": "54",
+  "MinTemp": "38",
+  "SunHours":"9.1",
+  "UVIndex":"5",
+  "TempDelta":"16",
+  "BarChange":"0.003",
+  "HeatChange":"0.022",
+  "HumChange":"-0.014"
+}, {
 
+  "Dates": "2021-04-18",
+  "Cloudcover": "62",
+  "Humidity": "72",
+  "Pressure": "1014",
+  "FeelsLike": "44",
+  "HeatIndex": "46",
+  "MaxTemp": "54",
+  "MinTemp": "38",
+  "SunHours":"9.1",
+  "UVIndex":"5",
+  "TempDelta":"16",
+  "BarChange":"0.003",
+  "HeatChange":"0.022",
+  "HumChange":"-0.014"
+}, {
 
+  "Dates": "2021-04-18",
+  "Cloudcover": "62",
+  "Humidity": "72",
+  "Pressure": "1014",
+  "FeelsLike": "44",
+  "HeatIndex": "46",
+  "MaxTemp": "54",
+  "MinTemp": "38",
+  "SunHours":"9.1",
+  "UVIndex":"5",
+  "TempDelta":"16",
+  "BarChange":"0.003",
+  "HeatChange":"0.022",
+  "HumChange":"-0.014"
+}, {
 
+  "Dates": "2021-04-18",
+  "Cloudcover": "62",
+  "Humidity": "72",
+  "Pressure": "1014",
+  "FeelsLike": "44",
+  "HeatIndex": "46",
+  "MaxTemp": "54",
+  "MinTemp": "38",
+  "SunHours":"9.1",
+  "UVIndex":"5",
+  "TempDelta":"16",
+  "BarChange":"0.003",
+  "HeatChange":"0.022",
+  "HumChange":"-0.014"
+}, {
 
-d3.json("/data.json", function(data) {
-    console.log(data);
-})
+  "Dates": "2021-04-18",
+  "Cloudcover": "62",
+  "Humidity": "72",
+  "Pressure": "1014",
+  "FeelsLike": "44",
+  "HeatIndex": "46",
+  "MaxTemp": "54",
+  "MinTemp": "38",
+  "SunHours":"9.1",
+  "UVIndex":"5",
+  "TempDelta":"16",
+  "BarChange":"0.003",
+  "HeatChange":"0.022",
+  "HumChange":"-0.014"
+}, {
 
-
-
-
-
+  "Dates": "2021-04-18",
+  "Cloudcover": "62",
+  "Humidity": "72",
+  "Pressure": "1014",
+  "FeelsLike": "44",
+  "HeatIndex": "46",
+  "MaxTemp": "54",
+  "MinTemp": "38",
+  "SunHours":"9.1",
+  "UVIndex":"5",
+  "TempDelta":"16",
+  "BarChange":"0.003",
+  "HeatChange":"0.022",
+  "HumChange":"-0.014"
+}]
 
 d3.select("pricing_table_wdg")
-  .selectAll("tr")
-  .data(austinWeather)
+  .selectAll("ul")
+  .data("data")
   .enter()
-  .append("tr")
+  .append("ul")
+  .append("li")
   .html(function(d) {
-    return `<td>${d.date}</td><td>${d.low}</td><td>${d.high}</td>`;
+    return `<li>${d.Dates}</li>
+            <li>${d.Clpudcover}</li>
+            <li>${d.Humidity}</li><li>${d.Pressure}</li>
+            <li>${d.FeelsLike}</li><li>${d.HeatIndex}</li>
+            <li>${d.MaxTemp}</li>
+            <li>${d.MinTemp}</li>
+            <li>${d.SunHours}</li>
+            <li>${d.UVIndex}</li>
+            <li>${d.TempDelta}</li>
+            <li>${d.BarChange}</li>
+            <li>${d.HeatChange}</li>
+            <li>${d.HumChange}</li>`;
   });
 
-
-
+  console.log(data)
